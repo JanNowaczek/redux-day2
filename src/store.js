b5a207adfaad7507c3b9d4bc836824b3e10f0ae2
mrozo2m <mrozo2m@interia.pl>
2019-04-16 10:30:31 +0200

    counter js INC and DEC added and exported to store

---
+++ src/store.js
@@ -0,0 +1,15 @@
+import { createStore, combineReducers } from 'redux'
+
+import counter, { incActionCreator, decActionCreator } from './state/counter'
+
+const rootReducer = combineReducers({
+    counter,
+})
+
+export const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+store.dispatch(incActionCreator())
+store.dispatch(decActionCreator())
